6a3ea70f844363fc02fb5af4006f3fdb04ced545
cwshkut <alexey.shkut@clockwise.software>
2023-01-11 13:05:05 +0000

    add missing file to git index

---
+++ src/entities/index.ts
@@ -0,0 +1,19 @@
+import { Node } from './Nodes';
+import { NodePropertyKey } from './NodePropertyKeys';
+import { Relationship } from './Relationships';
+import { NodeType } from './NodeTypes';
+import { NodePropertyValue } from './NodePropertyValues';
+import { RelationshipPropertyKey } from './RelationshipPropertyKeys';
+import { RelationshipPropertyValue } from './RelationshipPropertyValues';
+import { RelationshipType } from './RelationshipTypes';
+
+export const entities = [
+  Node,
+  NodeType,
+  NodePropertyKey,
+  Relationship,
+  NodePropertyValue,
+  RelationshipPropertyKey,
+  RelationshipPropertyValue,
+  RelationshipType,
+];
